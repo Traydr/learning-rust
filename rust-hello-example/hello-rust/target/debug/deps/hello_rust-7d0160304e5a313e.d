5a38ff02fdeeed16026bc59820fcde037a97c3c9
Traydr <Traydr@protonmail.com>
2022-02-10 23:32:47 +0100

    Simple Rust Hello World

---
+++ rust-hello-example/hello-rust/target/debug/deps/hello_rust-7d0160304e5a313e.d
@@ -0,0 +1,5 @@
+/home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/target/debug/deps/hello_rust-7d0160304e5a313e: src/main.rs
+
+/home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/target/debug/deps/hello_rust-7d0160304e5a313e.d: src/main.rs
+
+src/main.rs:
---
+++ rust-hello-example/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/target/debug/hello-rust: /home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/src/main.rs
