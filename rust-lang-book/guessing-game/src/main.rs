b3be879ec9679bd21b35bed7d7f9240efb33e1ad
Traydr <Traydr@protonmail.com>
2022-02-13 00:03:05 +0100

    Working on the rust lings guessing game

--- rust-lang-book/guessing-game/src/main.rs
+++ rust-lang-book/guessing-game/src/main.rs
@@ -1,10 +1,18 @@
 // Imports the standard io library
 use std::io;
+// Imports the rng crate
+use rang::Rng;
 
 // The main entry point to the program
 fn main() {
     // This macro prints to the screen
-    println!("Guess a number! (input below)");
+    println!("Guess a number!");
+
+    // Creates an immutable var with a range from 1 to 101
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Secret number: {}", secret_number);
+    println!("Input below");
 
     // Creates a variable changeable variables of type string
     let mut guess = String::new();
---
+++ rust-lang-book/guessing-game/target/debug/deps/cfg_if-d79d4230b67dff33.d
@@ -0,0 +1,7 @@
+/home/titas/Documents/Projects/learning-rust/rust-lang-book/guessing-game/target/debug/deps/cfg_if-d79d4230b67dff33.rmeta: /home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/titas/Documents/Projects/learning-rust/rust-lang-book/guessing-game/target/debug/deps/libcfg_if-d79d4230b67dff33.rlib: /home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/titas/Documents/Projects/learning-rust/rust-lang-book/guessing-game/target/debug/deps/cfg_if-d79d4230b67dff33.d: /home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
