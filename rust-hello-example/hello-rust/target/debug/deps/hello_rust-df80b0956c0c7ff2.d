a7e0b78cf96d284bdf43be6faae55460046b65b3
Traydr <64400491+Traydr@users.noreply.github.com>
2022-03-15 10:50:36 +0100

    Delete rust-hello-example/hello-rust/target directory

--- rust-hello-example/hello-rust/target/debug/deps/hello_rust-df80b0956c0c7ff2.d
+++
@@ -1,5 +0,0 @@
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/target/debug/deps/hello_rust-df80b0956c0c7ff2: src/main.rs
-
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/target/debug/deps/hello_rust-df80b0956c0c7ff2.d: src/main.rs
-
-src/main.rs:
--- rust-hello-example/hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/target/debug/hello-rust: /home/titas/Documents/Projects/learning-rust/rust-hello-example/hello-rust/src/main.rs
