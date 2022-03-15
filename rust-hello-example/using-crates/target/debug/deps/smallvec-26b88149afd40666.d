0c234833beb88a5d0123b5c4aebe756b75af443b
Traydr <64400491+Traydr@users.noreply.github.com>
2022-03-15 10:50:54 +0100

    Delete rust-hello-example/using-crates/target directory

--- rust-hello-example/using-crates/target/debug/deps/smallvec-26b88149afd40666.d
+++
@@ -1,7 +0,0 @@
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/using-crates/target/debug/deps/smallvec-26b88149afd40666.rmeta: /home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-0.4.5/lib.rs
-
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/using-crates/target/debug/deps/libsmallvec-26b88149afd40666.rlib: /home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-0.4.5/lib.rs
-
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/using-crates/target/debug/deps/smallvec-26b88149afd40666.d: /home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-0.4.5/lib.rs
-
-/home/titas/.cargo/registry/src/github.com-1ecc6299db9ec823/smallvec-0.4.5/lib.rs:
--- rust-hello-example/using-crates/target/debug/using-crates.d
+++
@@ -1,1 +0,0 @@
-/home/titas/Documents/Projects/learning-rust/rust-hello-example/using-crates/target/debug/using-crates: /home/titas/Documents/Projects/learning-rust/rust-hello-example/using-crates/src/main.rs
